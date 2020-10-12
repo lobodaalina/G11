bdc01c25a7f5d3644f4c8bfd815c594dca33a973
lobodaalina <68819644+lobodaalina@users.noreply.github.com>
2020-10-12 15:55:39 +0300

    Add files via upload

---
+++ string search.py
@@ -0,0 +1,13 @@
+string = input("Пишите тут:")
+a = input("Искомая буква:")
+cnt = 0
+
+
+def func(cnt):
+    for i in string:
+        if i == a:
+            cnt = cnt + 1
+    print("В строке буква 'а' встречается", cnt, "раз")
+
+
+func(cnt)
